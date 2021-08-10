9c4798f27094dc7c0c6ea80b705c19d3c57dea9f
Nicolay <nic.ilinov@gmail.com>
2021-08-10 16:13:19 +0700

    update step

--- step.sh
+++ step.sh
@@ -8,7 +8,7 @@ echo "${template_message_start}"
 
 curl --location --request POST 'https://api.telegram.org/'${telegram_bot_token}'/sendMessage' \
 --header 'Content-Type: application/json' \
---data-raw '{"chat_id": "'$TELEGRAM_ID_CHANNEL'", "text": $template_message_start, "disable_notification": false, "parse_mode": "HTML"}'
+--data-raw '{"chat_id": "'${telegram_id_channel}'", "text": $template_message_start, "disable_notification": false, "parse_mode": "HTML"}'
 
 
 #echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
