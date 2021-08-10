5375fb44a48acbd9b6a9f22bdbea86ec7e5f24bb
Nicolay <nic.ilinov@gmail.com>
2021-08-10 16:23:54 +0700

    update step

--- step.sh
+++ step.sh
@@ -3,35 +3,10 @@ set -ex
 
 echo "Start send message"
 
-echo "${telegram_bot_token}"
-echo "${template_message_start}"
-
 curl --location --request POST 'https://api.telegram.org/'${telegram_bot_token}'/sendMessage' \
 --header 'Content-Type: application/json' \
 --data-raw '{"chat_id": "'${telegram_id_channel}'", "text": ${template_message_start}, "disable_notification": false, "parse_mode": "HTML"}'
 
-curl --location --request POST 'postman-echo.com' \
+curl --location --request POST 'https://postman-echo.com' \
 --header 'Content-Type: application/json' \
 --data-raw '{"chat_id": "'${telegram_id_channel}'", "text": ${template_message_start}, "disable_notification": false, "parse_mode": "HTML", "bot_id": ${telegram_bot_token}}'
-
-
-
-#echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
-
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
-
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
