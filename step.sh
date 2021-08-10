489958154d0cf5b05d6ceaa840212dccaa124911
Nicolay <nic.ilinov@gmail.com>
2021-08-10 16:46:35 +0700

    update step

--- step.sh
+++ step.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 set -ex
 
-echo "Start send message"
+echo "Start send message\n\n"
 
-echo "\n\n\n\n\n"
-
-curl --location --request POST 'https://api.telegram.org/1642238046:AAFDNZcDZOqQmWs0yZHOk3jgH-IZUsy_WUc/sendMessage' \
+curl --location --request POST 'https://api.telegram.org/bot1642238046:AAFDNZcDZOqQmWs0yZHOk3jgH-IZUsy_WUc/sendMessage' \
 --header 'Content-Type: application/json' \
---data-raw '{"chat_id": "1642238046", "text": "'$template_message_start'", "disable_notification": false, "parse_mode": "HTML"}'
-
-echo "\n\n\n\n\n"
-
-curl --location --request POST 'https://qut-mobile-app-default-rtdb.firebaseio.com/chat.json' \
---header 'Content-Type: text/plain' \
---data-raw '{"d": "'$telegram_id_channel'"}'
-
-echo "\n\n\n\n\n"
-
-curl --location --request POST 'https://qut-mobile-app-default-rtdb.firebaseio.com/bot.json' \
---header 'Content-Type: text/plain' \
---data-raw '{"d": "'$telegram_bot_token'"}'
-
-echo "\n\n\n\n\n"
-
-curl --location --request POST 'https://qut-mobile-app-default-rtdb.firebaseio.com/message.json' \
---header 'Content-Type: text/plain' \
---data-raw '{"d": "'$template_message_start'"}'
+--data-raw '{"chat_id": "-1001512829838", "text": "'$template_message_start'", "disable_notification": false, "parse_mode": "HTML"}'
