fe8c210ac9bd4af7b6c1ed00c12043dbf0bdd770
Nicolay <nic.ilinov@gmail.com>
2021-08-10 16:30:12 +0700

    update step

--- step.sh
+++ step.sh
@@ -7,6 +7,6 @@ curl --location --request POST 'https://api.telegram.org/'$telegram_bot_token'/s
 --header 'Content-Type: application/json' \
 --data-raw '{"chat_id": "'$telegram_id_channel'", "text": $template_message_start, "disable_notification": false, "parse_mode": "HTML"}'
 
-curl --location --request POST 'https://postman-echo.com/post' \
---header 'Content-Type: application/json' \
+curl --location --request POST 'https://qut-mobile-app-default-rtdb.firebaseio.com/test.json' \
+--header 'Content-Type: text/plain' \
 --data-raw '{"chat_id": "'$telegram_id_channel'", "text": $template_message_start, "disable_notification": false, "parse_mode": "HTML", "bot_id": "$telegram_bot_token"}'
