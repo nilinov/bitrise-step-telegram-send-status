f5be78aa20708ec4fc7cf7cb00f77e80bbc69f77
Nicolay <nic.ilinov@gmail.com>
2021-08-10 16:25:12 +0700

    update step

--- step.sh
+++ step.sh
@@ -3,10 +3,10 @@ set -ex
 
 echo "Start send message"
 
-curl --location --request POST 'https://api.telegram.org/'${telegram_bot_token}'/sendMessage' \
+curl --location --request POST 'https://api.telegram.org/'$telegram_bot_token'/sendMessage' \
 --header 'Content-Type: application/json' \
---data-raw '{"chat_id": "'${telegram_id_channel}'", "text": ${template_message_start}, "disable_notification": false, "parse_mode": "HTML"}'
+--data-raw '{"chat_id": "'$telegram_id_channel'", "text": $template_message_start, "disable_notification": false, "parse_mode": "HTML"}'
 
 curl --location --request POST 'https://postman-echo.com' \
 --header 'Content-Type: application/json' \
---data-raw '{"chat_id": "'${telegram_id_channel}'", "text": ${template_message_start}, "disable_notification": false, "parse_mode": "HTML", "bot_id": ${telegram_bot_token}}'
+--data-raw '{"chat_id": "'$telegram_id_channel'", "text": $template_message_start, "disable_notification": false, "parse_mode": "HTML", "bot_id": "$telegram_bot_token"}'
