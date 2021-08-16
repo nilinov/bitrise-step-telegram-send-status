7e3fe6f0a963244b31d1932a9f607b894ea6a61b
Nicolay <nic.ilinov@gmail.com>
2021-08-16 16:21:49 +0700

    update info

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@ set -ex
 
 echo "Start send message\n\n"
 
-resJson=eval '{\"chat_id\": \"$telegram_id_channel\", \"text\": \"$template_message_start\", \"disable_notification\": false, \"parse_mode\": \"HTML\"}'
+resJson=eval "{\"chat_id\": \"$telegram_id_channel\", \"text\": \"$template_message_start\", \"disable_notification\": false, \"parse_mode\": \"HTML\"}"
 
 curl --location --request POST 'https://api.telegram.org/bot'$telegram_bot_token'/sendMessage' \
 --header 'Content-Type: application/json' \
