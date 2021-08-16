4cc1152756b8e46ba5d1458aca41420708df8c56
Nicolay <nic.ilinov@gmail.com>
2021-08-16 16:49:29 +0700

    добавил поддержку всех events

--- step.sh
+++ step.sh
@@ -3,6 +3,24 @@ set -ex
 
 echo "Start send message\n\n"
 
+message=$template_message_start;
+
+if [[ $type_notification = "End build" ]]
+then
+  if [[ $BITRISE_BUILD_STATUS = 0 ]]
+  then
+    message=$template_message_end_success;
+  else
+    message=$template_message_end_error;
+  fi
+fi
+
+if [[ $type_notification = "End build" ]]
+then
+    message=$template_message_custom;
+fi
+
+
 curl --location --request POST 'https://api.telegram.org/bot'$telegram_bot_token'/sendMessage' \
 --header 'Content-Type: application/json' \
---data-raw "{\"chat_id\": \"$telegram_id_channel\", \"text\": \"$template_message_start\", \"disable_notification\": false, \"parse_mode\": \"HTML\"}"
+--data-raw "{\"chat_id\": \"$telegram_id_channel\", \"text\": \"$message\", \"disable_notification\": false, \"parse_mode\": \"HTML\"}"
